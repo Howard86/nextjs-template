372760eab3e2ec94edf2a7e36b15bacae48ae84f
Howard Tai <howard@howardism.dev>
2023-03-25 09:58:26 +0000

    chore: bump all dependencies to latest except typescript

--- src/common/components/Image.spec.tsx
+++ src/common/components/Image.spec.tsx
@@ -5,7 +5,7 @@ import Image from './Image';
 describe('image', () => {
   it('should render a <img> tag', () => {
     expect.hasAssertions();
-    render(<Image src="MOCK_SRC" />);
+    render(<Image alt="MOCK_SRC" src="MOCK_SRC" />);
 
     expect(screen.getByRole('img')).toHaveAttribute('src', 'MOCK_SRC');
   });
