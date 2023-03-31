0afa622fbb8bdc91d666f8347cc9df05f9b28154
Howard Tai <howard@howardism.dev>
2023-03-31 21:30:55 +0800

    chore: update eslint & prettier config, npm scripts

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,8 @@
 /** @type{import('prettier').Config} */
 module.exports = {
-  useTabs: false,
   printWidth: 80,
-  tabWidth: 2,
   singleQuote: true,
-  trailingComma: 'all',
-  semi: true,
+  trailingComma: 'es5',
+  semi: false,
   bracketSpacing: true,
-};
+}
